a352a28ba80b647f999611bb54ebcb37574261f5
Karim Mourra <karim@jwplayer.com>
2022-05-26 16:22:10 -0400

    supports bundling

--- gulpHelpers.js
+++ gulpHelpers.js
@@ -11,6 +11,7 @@ const submodules = dependencyMap.parentModules;
 const libraries = dependencyMap.libraries;
 
 const MODULE_PATH = './modules';
+const LIBRARY_PATH = './libraries';
 const BUILD_PATH = './build/dist';
 const DEV_PATH = './build/dev';
 const ANALYTICS_PATH = '../analytics';
@@ -70,6 +71,12 @@ module.exports = {
       }
     });
 
+    Object.keys(libraries).forEach(library => {
+      if (!modules.includes(library) && modules.some(module => libraries[library].dependants.includes(module))) {
+        modules.unshift(library);
+      }
+    });
+
     return modules;
   },
   getParentLibraries(moduleName) {
@@ -91,33 +98,44 @@ module.exports = {
     externalModules = externalModules || [];
     var internalModules;
     try {
+      var getInternalModules = function(absolutePath) {
+        return fs.readdirSync(absolutePath)
+          .filter(file => (/^[^\.]+(\.js)?$/).test(file))
+          .reduce((memo, file) => {
+            var moduleName = file.split(new RegExp('[.\\' + path.sep + ']'))[0];
+            var modulePath = path.join(absolutePath, file);
+            if (fs.lstatSync(modulePath).isDirectory()) {
+              modulePath = path.join(modulePath, 'index.js')
+            }
+            if (fs.existsSync(modulePath)) {
+              memo[modulePath] = moduleName;
+            }
+            return memo;
+          }, {});
+      };
+
       var absoluteModulePath = path.join(__dirname, MODULE_PATH);
-      internalModules = fs.readdirSync(absoluteModulePath)
-        .filter(file => (/^[^\.]+(\.js)?$/).test(file))
-        .reduce((memo, file) => {
-          var moduleName = file.split(new RegExp('[.\\' + path.sep + ']'))[0];
-          var modulePath = path.join(absoluteModulePath, file);
-          if (fs.lstatSync(modulePath).isDirectory()) {
-            modulePath = path.join(modulePath, 'index.js')
-          }
-          if (fs.existsSync(modulePath)) {
-            memo[modulePath] = moduleName;
-          }
-          return memo;
-        }, {});
+      var absoluteLibraryPath = path.join(__dirname, LIBRARY_PATH);
+
+      internalModules = getInternalModules(absoluteModulePath);
+      var internalLibraries = getInternalModules(absoluteLibraryPath);
+      Object.assign(internalModules, internalLibraries);
     } catch (err) {
       internalModules = {};
     }
     return Object.assign(externalModules.reduce((memo, module) => {
       try {
         // prefer internal project modules before looking at project dependencies
-        var modulePath = require.resolve(module, {paths: ['./modules']});
-        if (modulePath === '') modulePath = require.resolve(module);
+        var modulePath = require.resolve(module, {paths: [MODULE_PATH, LIBRARY_PATH]});
+        if (modulePath === '') {
+          modulePath = require.resolve(module);
+        }
 
         memo[modulePath] = module;
       } catch (err) {
         // do something
       }
+
       return memo;
     }, internalModules));
   }),
