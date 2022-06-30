d8ed6d4d33894b14d0d96ef7711971fcd19ef206
Karim Mourra <karim@jwplayer.com>
2022-06-30 16:07:02 -0400

    var

--- libraries/video/index.js
+++ libraries/video/index.js
@@ -1,192 +1,142 @@
 import { config } from '../../src/config.js';
-import { find } from '../../src/polyfill.js';
-import * as events from '../../src/events.js';
-import { mergeDeep } from '../../src/utils.js';
-import { getGlobal } from '../../src/prebidGlobal.js';
+import events from '../../src/events.js';
+import { allVideoEvents } from './constants/events.js';
 import CONSTANTS from '../../src/constants.json';
-import {
-  videoEvents, AUCTION_AD_LOAD_ATTEMPT, AD_IMPRESSION, AD_ERROR, BID_IMPRESSION, BID_ERROR, AUCTION_AD_LOAD_ABORT
-} from './constants/events.js'
 import { videoCoreFactory } from './coreVideo.js';
-import { gamSubmoduleFactory } from './gamAdServerSubmodule.js';
-import { videoImpressionVerifierFactory } from './videoImpressionVerifier.js';
+import { coreAdServerFactory } from './adServer.js';
+import find from 'core-js-pure/features/array/find.js';
+import { vastXmlEditorFactory } from './shared/vastXmlEditor.js';
 
-const videoPrefix = 'video_';
+events.addEvents(allVideoEvents);
 
-const allVideoEvents = Object.keys(videoEvents).map(eventKey => videoEvents[eventKey]);
-events.addEvents(allVideoEvents.concat([AUCTION_AD_LOAD_ATTEMPT, AUCTION_AD_LOAD_ABORT, BID_IMPRESSION, BID_ERROR]).map(eventName => videoPrefix + eventName));
-
-/**
- * This module adds User Video support to prebid.js
- * @module modules/videoModule
- */
-export function PbVideo(videoCore_, getConfig_, pbGlobal_, pbEvents_, videoEvents_, gamAdServerFactory_, videoImpressionVerifierFactory_) {
+export function PbVideo(videoCore_, getConfig_, pbGlobal_, pbEvents_, videoEvents_, adServerCore_, vastXmlEditor_) {
   const videoCore = videoCore_;
   const getConfig = getConfig_;
   const pbGlobal = pbGlobal_;
   const requestBids = pbGlobal.requestBids;
   const pbEvents = pbEvents_;
   const videoEvents = videoEvents_;
-  const gamAdServerFactory = gamAdServerFactory_;
-  let gamSubmodule;
-  const videoImpressionVerifierFactory = videoImpressionVerifierFactory_;
-  let videoImpressionVerifier;
+  const adServerCore = adServerCore_;
+  const vastXmlEditor = vastXmlEditor_;
 
   function init() {
-    const cache = getConfig('cache');
-    videoImpressionVerifier = videoImpressionVerifierFactory(!!cache);
     getConfig('video', ({ video }) => {
       video.providers.forEach(provider => {
-        videoCore.registerProvider(provider);
-        videoCore.onEvents(videoEvents, (type, payload) => {
-          pbEvents.emit(videoPrefix + type, payload);
-        }, provider.divId);
+        try {
+          videoCore.registerProvider(provider);
+          videoCore.onEvents(videoEvents, (type, payload) => {
+            pbEvents.emit(type, payload);
+          }, provider.divId);
+        } catch (e) {}
 
         const adServerConfig = provider.adServer;
-        if (!gamSubmodule && adServerConfig) {
-          gamSubmodule = gamAdServerFactory();
+        if (adServerConfig) {
+          adServerCore.registerAdServer(adServerConfig.vendorCode, adServerConfig.params);
         }
       });
     });
 
-    requestBids.before(beforeBidsRequested, 40);
+    requestBids.before(enrichAdUnits, 40);
 
-    pbEvents.on(CONSTANTS.EVENTS.BID_ADJUSTMENT, function (bid) {
-      videoImpressionVerifier.trackBid(bid);
+    pbEvents.on(CONSTANTS.EVENTS.AUCTION_END, function(auctionResult) {
+      auctionResult.adUnits.forEach(adUnit => {
+        if (adUnit.video) {
+          renderWinningBid(adUnit);
+        }
+      });
     });
 
-    pbEvents.on(videoPrefix + AD_IMPRESSION, function (payload) {
-      triggerVideoBidEvent(BID_IMPRESSION, payload);
-    });
-
-    pbEvents.on(videoPrefix + AD_ERROR, function (payload) {
-      triggerVideoBidEvent(BID_ERROR, payload);
-    });
-  }
-
-  function renderBid(divId, bid, options = {}) {
-    const adUrl = bid.vastUrl;
-    options.adXml = bid.vastXml;
-    options.winner = bid.bidder;
-    loadAdTag(adUrl, divId, options);
-  }
-
-  return { init, renderBid };
-
-  function beforeBidsRequested(nextFn, bidRequest) {
-    const adUnits = bidRequest.adUnits || pbGlobal.adUnits || [];
-    adUnits.forEach(adUnit => {
-      enrichAdUnit(adUnit);
-    });
-
-    const bidsBackHandler = bidRequest.bidsBackHandler;
-    if (!bidsBackHandler || typeof bidsBackHandler !== 'function') {
-      pbEvents.on(CONSTANTS.EVENTS.AUCTION_END, auctionEnd);
+    const cache = getConfig('cache');
+    if (!cache) {
+      return;
     }
 
+    pbEvents.on(CONSTANTS.EVENTS.BID_ADJUSTMENT, function (bid) {
+      const adUnitCode = bid.adUnitCode;
+      const adUnit = find(pbGlobal.adUnits, adUnit => adUnitCode === adUnit.code);
+      const videoConfig = adUnit && adUnit.video;
+      const adServerConfig = videoConfig && videoConfig.adServer;
+      const trackingConfig = adServerConfig && adServerConfig.tracking;
+      addTrackingNodesToVastXml(bid, trackingConfig);
+    });
+  }
+
+  return { init };
+
+  function enrichAdUnits(nextFn, bidRequest) {
+    const adUnits = bidRequest.adUnits || pbGlobal.adUnits || [];
+    adUnits.forEach(adUnit => {
+      const oRtbParams = videoCore.getOrtbParams(adUnit.video.divId);
+      adUnit.mediaTypes.video = Object.assign({}, adUnit.mediaTypes.video, oRtbParams);
+    });
     return nextFn.call(this, bidRequest);
   }
 
-  function enrichAdUnit(adUnit) {
-    const videoMediaType = adUnit.mediaTypes.video;
-    const videoConfig = adUnit.video;
-    if (!videoMediaType || !videoConfig) {
-      return;
-    }
-
-    const oRtbParams = videoCore.getOrtbParams(videoConfig.divId);
-    if (!oRtbParams) {
-      return;
-    }
-
-    const ortbVideo = oRtbParams.video;
-    adUnit.mediaTypes.video = Object.assign({}, videoMediaType, ortbVideo);
-
-    adUnit.mediaTypes.video.context = ortbVideo.placement === 1 ? 'instream' : 'outstream';
-
-    const width = ortbVideo.w;
-    const height = ortbVideo.h;
-    if (width && height) {
-      adUnit.mediaTypes.video.playerSize = [width, height];
-    }
-
-    let ortb2 = { ortb2: mergeDeep({}, getConfig('ortb2'), { site: oRtbParams.content }) };
-    pbGlobal.setConfig(ortb2);
-  }
-
-  function auctionEnd(auctionResult) {
-    auctionResult.adUnits.forEach(adUnit => {
-      if (adUnit.video) {
-        renderWinningBid(adUnit);
-      }
-    });
-  }
-
   function renderWinningBid(adUnit) {
-    const adUnitCode = adUnit.code;
-    const options = { adUnitCode };
-
     const videoConfig = adUnit.video;
     const divId = videoConfig.divId;
     const adServerConfig = videoConfig.adServer;
-    let adUrl;
+    let adTagUrl;
     if (adServerConfig) {
-      adUrl = gamSubmodule.getAdTagUrl(adUnit, adServerConfig.baseAdTagUrl, adServerConfig.params);
+      adTagUrl = adServerCore.getAdTagUrl(adServerConfig.vendorCode, adUnit, adServerConfig.baseAdTagUrl);
     }
 
-    if (adUrl) {
-      loadAdTag(adUrl, divId, options);
+    const adUnitCode = adUnit.code;
+    const options = { adUnitCode };
+    if (adTagUrl) {
+      videoCore.setAdTagUrl(adTagUrl, divId, options);
       return;
     }
 
-    const highestCpmBids = pbGlobal.getHighestCpmBids(adUnitCode);
-    if (!highestCpmBids.length) {
-      pbEvents.emit(videoPrefix + AUCTION_AD_LOAD_ABORT, options);
-      return;
-    }
-
-    const highestBid = highestCpmBids.shift();
+    const highestCpmBids = pbGlobal.getHighestCpmBids(adUnit.code);
+    const highestBid = highestCpmBids && highestCpmBids.shift();
     if (!highestBid) {
       return;
     }
 
-    renderBid(divId, highestBid, options);
+    adTagUrl = highestBid.vastUrl;
+    options.adXml = highestBid.vastXml;
+    videoCore.setAdTagUrl(adTagUrl, divId, options);
   }
 
-  // options: adXml, winner, adUnitCode,
-  function loadAdTag(adUrl, divId, options) {
-    const payload = Object.assign({ adUrl }, options);
-    pbEvents.emit(videoPrefix + AUCTION_AD_LOAD_ATTEMPT, payload);
-    videoCore.setAdTagUrl(adUrl, divId, options);
-  }
-
-  function triggerVideoBidEvent(eventName, adEventPayload) {
-    const bid = getBid(adEventPayload);
-    if (!bid) {
-      return;
-    }
-    pbEvents.emit(videoPrefix + eventName, { bid, adEvent: adEventPayload });
-  }
-
-  function getBid(adPayload) {
-    const { adId, adTagUrl, wrapperAdIds } = adPayload;
-    const bidIdentifiers = videoImpressionVerifier.getBidIdentifiers(adId, adTagUrl, wrapperAdIds);
-    if (!bidIdentifiers) {
+  function addTrackingNodesToVastXml(bid, trackingConfig) {
+    if (!trackingConfig) {
       return;
     }
 
-    const { bidAdId = adId, adUnitCode, requestId, auctionId } = bidIdentifiers;
-    const { bids } = pbGlobal.getBidResponsesForAdUnitCode(adUnitCode);
-    return find(bids, bid => bid.adId === bidAdId && bid.requestId === requestId && bid.auctionId === auctionId);
+    let { vastXml, vastUrl, adId } = bid;
+    let impressionUrl;
+    let impressionId;
+    let errorUrl;
+
+    const impressionTracking = trackingConfig.impression;
+    const errorTracking = trackingConfig.error;
+
+    if (impressionTracking) {
+      impressionUrl = impressionTracking.url;
+      impressionId = impressionTracking.id || adId + '-impression';
+    }
+
+    if (errorTracking) {
+      errorUrl = errorTracking.url;
+    }
+
+    if (vastXml) {
+      vastXml = vastXmlEditor.getVastXmlWithTrackingNodes(vastXml, impressionUrl, impressionId, errorUrl);
+    } else if (vastUrl) {
+      vastXml = vastXmlEditor.buildVastWrapper(adId, vastUrl, impressionUrl, impressionId, errorUrl);
+    }
+
+    bid.vastXml = vastXml;
   }
 }
 
 export function pbVideoFactory() {
   const videoCore = videoCoreFactory();
-  const pbGlobal = getGlobal();
-  const pbVideo = PbVideo(videoCore, config.getConfig, pbGlobal, events, allVideoEvents, gamSubmoduleFactory, videoImpressionVerifierFactory);
+  const adServerCore = coreAdServerFactory();
+  const vastXmlEditor = vastXmlEditorFactory();
+  const pbVideo = PbVideo(videoCore, config.getConfig, $$PREBID_GLOBAL$$, events, allVideoEvents, adServerCore, vastXmlEditor);
   pbVideo.init();
-  pbGlobal.videoModule = pbVideo;
   return pbVideo;
 }
 
